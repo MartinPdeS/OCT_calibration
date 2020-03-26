1b59dd30f6e3af1b0d087452da3844d193e6b319
Martth <Martin.poinsinet.de.sivry@gmail.com>
2020-03-26 16:22:42 -0400

    up

--- src/toolbox/_arguments.py
+++ src/toolbox/_arguments.py
@@ -1,3 +1,3 @@
 from argparse import Namespace 
 global Arguments 
-Arguments = Namespace(calibration_file='data/calibration/example/calib.json', dispersion=1, gpu=True, input_file='data/Bscan/example.npy', mean_number=1, shift=False, silent=False)
+Arguments = Namespace(calibration_file='data/calibration/example/calib.json', dispersion=1, gpu=True, input_file='data/Bscan/example.npy', mean_number=1, shift=False, silent=True)
